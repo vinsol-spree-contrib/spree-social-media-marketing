b1eb0170a5dab27a9bf2fb155f98b1683f1c9c15
Gaurav Mahajan <gaurav@vinsol.com>
2016-05-16 13:30:43 +0530

    change accessibility of 'get_and_assing_page_access_token' in facebook_page model to private. remove link from facebook_page.post_image set_condition for after initialize.

--- app/models/spree/facebook_page.rb
+++ app/models/spree/facebook_page.rb
@@ -5,33 +5,33 @@ module Spree
 
     validates :page_id, uniqueness: {message: 'has already been added'}
     before_validation :get_and_assing_page_access_token
-    after_initialize :set_koala_client
+    after_initialize :set_koala_client, if: :page_token?
 
     attr_accessor :client
-
-    def get_and_assing_page_access_token
-      page_access_token_uri = URI("https://graph.facebook.com/#{ page_id }")
-      page_access_token_uri.query = URI.encode_www_form(access_token: account.auth_token, fields: 'access_token')
-      response = Net::HTTP.get_response(page_access_token_uri)
-      response_json = JSON.parse(response.body)
-      if response.code == '200'
-        self.page_token = response_json['access_token']
-      else
-        errors.add(:base, response_json['error']['message'])
-      end
-    end
 
     def post_message(message)
       client.put_connections(self.page_id, 'feed', message: message)
     end
 
     def post_image(source_binary, caption = '')
-      client.put_picture(source_binary, 'multipart/form-data', {message: caption, link: 'http://google.com'}, self.page_id)
+      client.put_picture(source_binary, 'multipart/form-data', {message: caption}, self.page_id)
     end
 
     private
       def set_koala_client
         self.client = Koala::Facebook::API.new(self.page_token)
+      end
+
+      def get_and_assing_page_access_token
+        page_access_token_uri = URI("https://graph.facebook.com/#{ page_id }")
+        page_access_token_uri.query = URI.encode_www_form(access_token: account.auth_token, fields: 'access_token')
+        response = Net::HTTP.get_response(page_access_token_uri)
+        response_json = JSON.parse(response.body)
+        if response.code == '200'
+          self.page_token = response_json['access_token']
+        else
+          errors.add(:base, response_json['error']['message'])
+        end
       end
   end
 end
